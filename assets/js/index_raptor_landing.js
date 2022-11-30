390b062f590684c1eb6b58c47dc23e9729977a3d
LanCodeEasy <plancerio@easygosa.net>
2022-11-30 15:11:47 -0600

    boton de cargar más en la galeria y iconos dinamicos en las preguntas

--- assets/js/index_raptor_landing.js
+++ assets/js/index_raptor_landing.js
@@ -1,6 +1,21 @@
+/* -------------------------Iconos dinamicos de las preguntas------------------------- */
 $(".preguntas__container--summary").click(function () {
   $(this)
     .children("i")
     .toggleClass("bi-dash-circle")
     .toggleClass("bi-plus-circle");
 });
+
+/* -------------------------Botón de cargar más------------------------- */
+let currentItem = 3;
+
+$('#load-more').click(function(){
+  let cajas = [...$('.galeria__container--box')];
+  for (var i = currentItem; i < currentItem + 3; i++) {
+    cajas[i].style.display = 'flex';
+  }
+  currentItem += 3;
+  if(currentItem >= cajas.length) {
+    $('#load-more').css('display', 'none');
+  }
+})
