985427708dce45df9de94ef237e3025fd8ec744e
LanCodeEasy <plancerio@easygosa.net>
2022-12-03 22:57:28 -0600

    Fechas validacion

--- assets/js/index_raptor_landing.js
+++ assets/js/index_raptor_landing.js
@@ -7,15 +7,22 @@ $(".preguntas__container--summary").click(function () {
 });
 
 /* -------------------------Botón de cargar más------------------------- */
-let currentItem = 3;
+// let currentItem = 3;
 
-$('#load-more').click(function(){
-  let cajas = [...$('.galeria__container--box')];
-  for (var i = currentItem; i < currentItem + 3; i++) {
-    cajas[i].style.display = 'flex';
-  }
-  currentItem += 3;
-  if(currentItem >= cajas.length) {
-    $('#load-more').css('display', 'none');
-  }
-})
+// $('#load-more').click(function(){
+//   let cajas = [...$('.galeria__container--box')];
+//   for (var i = currentItem; i < currentItem + 3; i++) {
+//     cajas[i].style.display = 'flex';
+//   }
+//   currentItem += 3;
+//   if(currentItem >= cajas.length) {
+//     $('#load-more').css('display', 'none');
+//   }
+// })
+
+// let hoyDia = new Date();
+// let dia = hoyDia.getDate();
+// var mes = hoyDia.getMonth() + 1;
+// var año = hoyDia.getFullYear();
+
+// console.log(`${dia}/${mes}/${año}`);
